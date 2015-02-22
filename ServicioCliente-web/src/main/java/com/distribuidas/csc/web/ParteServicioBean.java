4594d1542dfb4b09beb4b85c467d38b9ddff71b0
sbenalcazar <sbenalcazar@DECISION-032>
2015-02-21 22:04:27 -0500

    Parte Servicio

--- ServicioCliente-web/src/main/java/com/distribuidas/csc/web/ParteServicioBean.java
+++ ServicioCliente-web/src/main/java/com/distribuidas/csc/web/ParteServicioBean.java
@@ -5,14 +5,19 @@
  */
 package com.distribuidas.csc.web;
 
+import com.distribuidas.csc.persistencia.Contacto;
 import com.distribuidas.csc.persistencia.ParteServicio;
+import com.distribuidas.csc.persistencia.SolicitudServicio;
+import com.distribuidas.csc.persistencia.Tecnico;
+import com.distribuidas.csc.servicio.ContactoServicio;
 import com.distribuidas.csc.servicio.ParteServicioServicio;
+import com.distribuidas.csc.servicio.SolicitudServicioServicio;
+import com.distribuidas.csc.servicio.TecnicoServicio;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.model.SelectItem;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
 
@@ -26,8 +31,20 @@ public class ParteServicioBean {
     
     @EJB
     private ParteServicioServicio parteServicioServicio;
+    
+    @EJB
+    private ContactoServicio contactoServicio;
+    
+    @EJB 
+    private SolicitudServicioServicio solicitudServicioServicio;
+    
+    @EJB
+    private TecnicoServicio tecnicoServicio;
 
     private List<ParteServicio> parteServicios;
+    private List<Contacto> contactos;
+    private List<SolicitudServicio> solicitudServicios;
+    private List<Tecnico> tecnicos;
     private ParteServicio parteServicio;
     private ParteServicio parteServicioSeleccionado;
 
@@ -188,6 +205,31 @@ public class ParteServicioBean {
     public void setActivarCambiarEstado(Boolean activarCambiarEstado) {
         this.activarCambiarEstado = activarCambiarEstado;
     }
+
+    public List<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(List<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+    public List<SolicitudServicio> getSolicitudServicios() {
+        return solicitudServicios;
+    }
+
+    public void setSolicitudServicios(List<SolicitudServicio> solicitudServicios) {
+        this.solicitudServicios = solicitudServicios;
+    }
+
+    public List<Tecnico> getTecnicos() {
+        return tecnicos;
+    }
+
+    public void setTecnicos(List<Tecnico> tecnicos) {
+        this.tecnicos = tecnicos;
+    }
+    
     
     
 }
